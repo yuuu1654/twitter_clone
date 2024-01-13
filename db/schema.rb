a8453103d1a207064d220e555037bcdfb77ac43d
morishitayuuki <morishita@heartorg.co.jp>
2024-01-13 21:51:05 +0900

    [Add] deviseでユーザーモデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_06_140511) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_13_065349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,26 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_140511) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "phone_number", null: false
+    t.string "introduction"
+    t.string "place"
+    t.string "website"
+    t.datetime "birthday", null: false
+    t.string "profile_image"
+    t.string "avatar_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
