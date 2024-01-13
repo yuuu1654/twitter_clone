da542e097be4b9c617ac2db938a45e2e60cb5f3c
morishitayuuki <morishita@heartorg.co.jp>
2024-01-13 20:30:19 +0900

    [Add] Renderへのデプロイ設定ファイル 追加

---
+++ render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit # 途中でエラーが発生した時に残りのコマンドが実行されるのを防ぐ
+
+
+bundle install
+rails assets:precompile
+rails assets:clean
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:reset
+rails db:migrate
+# rails db:seed
+
+# Renderコンソールに設定用のコマンド▼
+# bundle install && rails assets:precompile && rails assets:clean && rails db:migrate && rails db:seed && rake promotion_code:generate
+# bundle install && rails assets:precompile && rails assets:clean && rails db:migrate
