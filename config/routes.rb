d6f7cb27c6b7a1643525756a28a69e4c28d036b6
morishitayuuki <morishita@heartorg.co.jp>
2024-01-16 14:57:23 +0900

    [Update] トップページの雛形 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'home/top'
+  root 'home#top'
   # devise_for :users # Deviseのデフォルトのルーティング
 
   devise_for :users, controllers: {
