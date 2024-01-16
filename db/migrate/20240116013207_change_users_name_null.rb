4a6fb1537c5480c37d62b66bcfb7f2f6d24fd811
morishitayuuki <morishita@heartorg.co.jp>
2024-01-16 11:23:58 +0900

    [Update] ユーザー名はnullを許容するよう修正

---
+++ db/migrate/20240116013207_change_users_name_null.rb
@@ -0,0 +1,5 @@
+class ChangeUsersNameNull < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :name, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_13_065349) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_013207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_13_065349) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
-    t.string "name", null: false
+    t.string "name"
     t.string "phone_number", null: false
     t.string "introduction"
     t.string "place"
