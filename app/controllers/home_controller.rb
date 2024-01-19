b367474682a054a37e8b6e9f88b526ba3a42243c
morishitayuuki <morishita@heartorg.co.jp>
2024-01-19 11:30:40 +0900

    作業途中

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def top
+    @current_user = current_user
+    logger.debug "ログインユーザー情報: #{@current_user.inspect}"
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,19 +5,20 @@ module Users
     # before_action :configure_sign_in_params, only: [:create]
 
     # GET /resource/sign_in
-    # def new
-    #   super
-    # end
+    def new
+      super
+    end
 
     # POST /resource/sign_in
-    # def create
-    #   super
-    # end
+    def create
+      super
+    end
 
     # DELETE /resource/sign_out
-    # def destroy
-    #   super
-    # end
+    def destroy
+      super
+      # redirect_to new_user_registration_path
+    end
 
     # protected
 
