f95196b766e1e2b332a7cd9811ab5d28c51f2014
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-01-23 16:33:20 +0900

    [Fix] ユーザー登録時にメール認証が不要のため修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,9 +11,14 @@ module Users
     # end
 
     # # POST /resource
-    # def create
-    #   super
-    # end
+    def create
+      super do
+        # resource.update(confirmed_at: Time .now.utc)       # Welcomeメールを送信した上で、skip_confirmation!と同一処理を行い自動で認証クローズさせる
+        #↓と同じ意味
+        resource.skip_confirmation!
+        resource.save
+      end
+    end
 
     # GET /resource/edit
     # def edit
