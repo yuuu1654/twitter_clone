840325fb12d05c761aeb8514c515aae52558c0cd
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-01-25 13:17:27 +0900

    [Fix] 本番環境のメール送信に必要なホストを指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # メールに関連するホストの設定
+  config.action_mailer.default_url_options = { host: 'https://x-clone-web.onrender.com' }
 end
