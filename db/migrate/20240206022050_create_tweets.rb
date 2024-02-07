9e696cc3bd7daa3363e4d33c6c8029260588fbc8
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-07 18:30:06 +0900

    [Add] Tweetモデルの雛形 作成

---
+++ db/migrate/20240206022050_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tweets do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_045057) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
