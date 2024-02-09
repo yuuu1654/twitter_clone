87f9e1ebc4cac79ac1ffa95baee0bd25c92a5dc4
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-09 12:04:37 +0900

    [Update] ツイートのデータを取得

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,12 @@ class HomeController < ApplicationController
   def index
     @current_user = current_user
     logger.debug "ログインユーザー情報: #{@current_user.inspect}"
+    @tweet = Tweet.new
+
+    # おすすめツイート
+    @recommended_tweets = Tweet.all.order(created_at: :desc)
+
+    # フォローしてるユーザーのツイート一覧
+    @following_tweets = Tweet.where(user_id: 14)
   end
 end
