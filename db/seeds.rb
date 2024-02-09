b2e7f260a1df37da1f0f13e94db53d1cbdd8cbfb
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-09 22:20:01 +0900

    [Update] 本番のテストデータ作成用にseedデータ修正v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,52 @@
 # TODO: フォロー機能作る時に関連性を整理する
 # ユーザー作成
-10.times do |n|
-  name  = "user#{n+10}"
-  email = "example-#{n+10}@gmail.com"
-	password = Devise.friendly_token[0, 20]
-	birthday = Date.new(2000, 1, 1) 
-	phone_number = "090#{rand(10_000_000..99_999_999)}"
-	uid = User.create_unique_string
-  User.create!(name: name,
-              email: email,
-              password: password,
-              birthday: birthday,
-              phone_number: phone_number,
-							uid: uid)
-end
 
-# ツイート作成
+# テスト用ユーザー1 (自分)
+name  = "本番テスト用1"
+email = "test-prd01@gmail.com"
+password = Devise.friendly_token[0, 20]
+birthday = Date.new(2000, 1, 1) 
+phone_number = "090#{rand(10_000_000..99_999_999)}"
+uid = User.create_unique_string
+User.create!(name:  name,
+            email: email,
+            password: password,
+            birthday: birthday,
+            phone_number: phone_number,
+            uid: uid)
+
+# テスト用ユーザー2 (フォロー対象ユーザー)
+name  = "本番テスト用2"
+email = "test-prd02@gmail.com"
+password = Devise.friendly_token[0, 20]
+birthday = Date.new(2000, 1, 1) 
+phone_number = "090#{rand(10_000_000..99_999_999)}"
+uid = User.create_unique_string
+User.create!(name:  name,
+            email: email,
+            password: password,
+            birthday: birthday,
+            phone_number: phone_number,
+            uid: uid)
+
+# 10.times do |n|
+#   name  = "user#{n+10}"
+#   email = "example-#{n+10}@gmail.com"
+# 	password = Devise.friendly_token[0, 20]
+# 	birthday = Date.new(2000, 1, 1) 
+# 	phone_number = "090#{rand(10_000_000..99_999_999)}"
+# 	uid = User.create_unique_string
+#   User.create!(name: name,
+#               email: email,
+#               password: password,
+#               birthday: birthday,
+#               phone_number: phone_number,
+# 							uid: uid)
+# end
+
+# ツイート作成 (フォローしてない人)
 10.times do |n|
-	user_id = 14
+	user_id = 17
 	sample_sentences = [
     "今日は良い天気ですね。",
     "最近読んだ本が面白かった。",
@@ -28,7 +57,8 @@ end
     "音楽を聴きながら勉強するのが好き。",
     "旅行の計画を立て始めた。",
     "美味しいコーヒーの淹れ方を学びたい。",
-    "運動することで気分が良くなった。"
+    "運動することで気分が良くなった。",
+    "サッカーと野球どっち派？もしかして卓球派？"
   ]
   # サンプルツイートからランダムに一つ選ぶ
   content = sample_sentences.sample
@@ -36,8 +66,9 @@ end
 								content: content)
 end
 
+# ツイート作成 (フォローしてる人)
 10.times do |n|
-	user_id = 35
+	user_id = User.find_by(email: "test-prd02@gmail.com").id
 	sample_sentences = [
     "今日は良い天気ですね。",
     "最近読んだ本が面白かった。",
@@ -48,7 +79,8 @@ end
     "音楽を聴きながら勉強するのが好き。",
     "旅行の計画を立て始めた。",
     "美味しいコーヒーの淹れ方を学びたい。",
-    "運動することで気分が良くなった。"
+    "運動することで気分が良くなった。",
+    "サッカーと野球どっち派？もしかして卓球派？"
   ]
   # サンプルツイートからランダムに一つ選ぶ
   content = sample_sentences.sample
@@ -58,17 +90,11 @@ end
 
 # ユーザー
 users = User.all
-user1 = User.find_by(id: 13)
-user2 = User.find_by(id: 14)
+test_user = User.find_by(email: "test-prd01@gmail.com")
+followed_user = User.find_by(email: "test-prd02@gmail.com")
 
-# すべてのユーザーをループし、user1がそれぞれをフォローするように設定
-users.each do |user|
-  # user1が自分自身をフォローしないようにチェック
-  unless user == user1
-    # フォロー関係を作成または検索し、既に存在しない場合にのみ作成
-    Follow.find_or_create_by(follower_id: user1.id, followed_id: user.id)
-  end
-end
+# フォロー関係作成
+Follow.find_or_create_by(follower_id: test_user.id, followed_id: followed_user.id)
 
 # フォロー機能を作る時に実装
 # users = User.all
