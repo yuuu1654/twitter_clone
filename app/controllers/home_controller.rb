134f77df593f527491836c1c35a7899243ad8995
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-10 14:11:29 +0900

    [Refactor] N+1問題解消

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,11 @@ class HomeController < ApplicationController
     @tweet = Tweet.new
 
     # おすすめツイート
-    @recommended_tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    @recommended_tweets = Tweet.includes(:user).all.order(created_at: :desc).page(params[:page]).per(10)
 
     # フォローしてるユーザーのツイート一覧 (本番テストの為、一時的に直でidを指定 <- フォロー機能実装時に修正)
     followed_user_id = User.find_by(email: "test-prd02@gmail.com")
     # @following_tweets = Tweet.where(user_id: 14).page(params[:page]).per(10)
-    @following_tweets = Tweet.where(user_id: followed_user_id).page(params[:page]).per(10)
+    @following_tweets = Tweet.includes(:user).where(user_id: followed_user_id).page(params[:page]).per(10)
   end
 end
