293395c95f0fb1894e2435924d9dca9767dda725
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-10 12:28:22 +0900

    [Update] 本番でフォローツイート一覧を確認するために一時的にデータ取得部分を修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
     # おすすめツイート
     @recommended_tweets = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
 
-    # フォローしてるユーザーのツイート一覧
-    @following_tweets = Tweet.where(user_id: 14).page(params[:page]).per(10)
+    # フォローしてるユーザーのツイート一覧 (本番テストの為、一時的に直でidを指定 <- フォロー機能実装時に修正)
+    followed_user_id = User.find_by(email: "test-prd02@gmail.com")
+    # @following_tweets = Tweet.where(user_id: 14).page(params[:page]).per(10)
+    @following_tweets = Tweet.where(user_id: followed_user_id).page(params[:page]).per(10)
   end
 end
