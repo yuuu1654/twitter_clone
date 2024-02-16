2a41acd0647a06d607dc5644cce48feea5b62392
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-16 19:39:04 +0900

    [Refactor] rubocopエラーの為、showメソッドの処理を切り出した (まだリファクタの余地あり?)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,25 @@ class UsersController < ApplicationController
   before_action :current_user, only: [:show]
   before_action :set_user, only: %i[show edit]
 
+  # def show ← rubocopエラーの為リファクタ
+  #   # ツイートがない時にもtotal_pagesメソッドを実行してエラーが出ないようにする
+  #   @my_tweets ||= Tweet.none.page(params[:page])
+  #   @liked_tweets ||= Tweet.none.page(params[:page])
+  #   @retweeted_tweets ||= Tweet.none.page(params[:page])
+  #   @commented_tweets ||= Tweet.none.page(params[:page])
+  #   case params[:tab]
+  #   when 'liked_tweets'
+  #     @liked_tweets = Tweet.where(content: 'いいねした投稿です').page(params[:page]).per(10)
+  #   when 'retweeted_tweets'
+  #     @retweeted_tweets = Tweet.where(content: 'リツイートした投稿です').page(params[:page]).per(10)
+  #   when 'commented_tweets'
+  #     @commented_tweets = Tweet.where(content: 'コメントした投稿です').page(params[:page]).per(10)
+  #   else
+  #     @my_tweets = Tweet.my_tweets(@user.id, params[:page])
+  #   end
+  # end
   def show
-    # ツイートがない時にもtotal_pagesメソッドを実行してエラーが出ないようにする
-    @my_tweets ||= Tweet.none.page(params[:page])
-    @liked_tweets ||= Tweet.none.page(params[:page])
-    @retweeted_tweets ||= Tweet.none.page(params[:page])
-    @commented_tweets ||= Tweet.none.page(params[:page])
-    case params[:tab]
-    when 'liked_tweets'
-      @liked_tweets = Tweet.where(content: 'いいねした投稿です').page(params[:page]).per(10)
-    when 'retweeted_tweets'
-      @retweeted_tweets = Tweet.where(content: 'リツイートした投稿です').page(params[:page]).per(10)
-    when 'commented_tweets'
-      @commented_tweets = Tweet.where(content: 'コメントした投稿です').page(params[:page]).per(10)
-    else
-      @my_tweets = Tweet.my_tweets(@user.id, params[:page])
-    end
+    set_specific_tweets
   end
 
   def edit; end
@@ -42,5 +45,44 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :phone_number, :birthday, :website, :place, :profile_image,
                                  :avatar_image, :password, :password_confirmation)
+  end
+
+  # ツイートコレクションの初期設定
+  def set_tweet_collections
+    @my_tweets = Tweet.none.page(params[:page])
+    @liked_tweets = Tweet.none.page(params[:page])
+    @retweeted_tweets = Tweet.none.page(params[:page])
+    @commented_tweets = Tweet.none.page(params[:page])
+  end
+
+  # タブに応じたツイートのセット
+  def set_specific_tweets
+    set_tweet_collections
+    case params[:tab]
+    when 'liked_tweets'
+      fetch_liked_tweets
+    when 'retweeted_tweets'
+      fetch_retweeted_tweets
+    when 'commented_tweets'
+      fetch_commented_tweets
+    else
+      fetch_my_tweets
+    end
+  end
+
+  def fetch_liked_tweets
+    @liked_tweets = Tweet.where(content: 'いいねした投稿です').page(params[:page]).per(10)
+  end
+
+  def fetch_retweeted_tweets
+    @retweeted_tweets = Tweet.where(content: 'リツイートした投稿です').page(params[:page]).per(10)
+  end
+
+  def fetch_commented_tweets
+    @commented_tweets = Tweet.where(content: 'コメントした投稿です').page(params[:page]).per(10)
+  end
+
+  def fetch_my_tweets
+    @my_tweets = Tweet.my_tweets(@user.id, params[:page])
   end
 end
