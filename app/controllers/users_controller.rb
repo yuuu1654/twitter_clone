c394a37452625cc91dd5dd2f145d6227d44abe95
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-16 17:14:37 +0900

    [Update] 自分の投稿・いいね/リツイート/コメントした投稿をタブで切り替えて表示するように修正 (ページネーションも導入)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
   before_action :current_user, only: [:show]
   before_action :set_user, only: %i[show edit]
 
-  def show
-    @my_tweets = Tweet.includes(:user).where(user_id: @user.id).order(created_at: :desc)
-    logger.debug "ログインユーザー: #{@user.inspect}"
-    logger.debug "自分のツイート一覧: #{@my_tweets.inspect}"
+  def show    
+    # ツイートがない時にもtotal_pagesメソッドを実行してエラーが出ないようにする
+    @my_tweets ||= Tweet.none.page(params[:page])
+    @liked_tweets ||= Tweet.none.page(params[:page])
+    @retweeted_tweets ||= Tweet.none.page(params[:page])
+    @commented_tweets ||= Tweet.none.page(params[:page])
+    if params[:tab] == "liked_tweets"
+      @liked_tweets = Tweet.where(content: "いいねした投稿です").page(params[:page]).per(10)
+    elsif params[:tab] == "retweeted_tweets"
+      @retweeted_tweets = Tweet.where(content: "リツイートした投稿です").page(params[:page]).per(10)
+    elsif params[:tab] == "commented_tweets"
+      @commented_tweets = Tweet.where(content: "コメントした投稿です").page(params[:page]).per(10)
+    else
+      @my_tweets = Tweet.my_tweets(@user.id, params[:page])
+    end
   end
 
   def edit; end
