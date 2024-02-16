25a6f7bc02bfef53ce47d493c336a53ccbdb93d2
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-16 09:27:54 +0900

    [Refactor] format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
 
-	private
+  def logged_in_user?
+    return if user_signed_in?
 
-	def logged_in_user?
-		if !user_signed_in?
-			redirect_to new_user_session_path
-		end
-	end
+    redirect_to new_user_session_path
+  end
 end
