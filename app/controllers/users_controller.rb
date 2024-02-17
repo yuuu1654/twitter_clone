256b5a4a58611162491467900eb1a4a0e4940862
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-17 15:38:33 +0900

    [Refactor] 不要コード削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,6 @@ class UsersController < ApplicationController
   before_action :current_user, only: [:show]
   before_action :set_user, only: %i[show edit]
 
-  # def show ← rubocopエラーの為リファクタ
-  #   # ツイートがない時にもtotal_pagesメソッドを実行してエラーが出ないようにする
-  #   @my_tweets ||= Tweet.none.page(params[:page])
-  #   @liked_tweets ||= Tweet.none.page(params[:page])
-  #   @retweeted_tweets ||= Tweet.none.page(params[:page])
-  #   @commented_tweets ||= Tweet.none.page(params[:page])
-  #   case params[:tab]
-  #   when 'liked_tweets'
-  #     @liked_tweets = Tweet.where(content: 'いいねした投稿です').page(params[:page]).per(10)
-  #   when 'retweeted_tweets'
-  #     @retweeted_tweets = Tweet.where(content: 'リツイートした投稿です').page(params[:page]).per(10)
-  #   when 'commented_tweets'
-  #     @commented_tweets = Tweet.where(content: 'コメントした投稿です').page(params[:page]).per(10)
-  #   else
-  #     @my_tweets = Tweet.my_tweets(@user.id, params[:page])
-  #   end
-  # end
   def show
     set_specific_tweets
   end
