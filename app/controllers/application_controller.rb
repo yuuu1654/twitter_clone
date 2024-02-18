48ee71e6bdb089232e905b2062c309f856963b28
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-18 12:44:44 +0900

    [Refactor] トップページ描画時に必要なツイートデータをセットする処理を共通化 (他のコントローラでも使用する為)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,18 @@ class ApplicationController < ActionController::Base
 
   def logged_in_user?
     return if user_signed_in?
-
     redirect_to new_user_session_path
+  end
+
+  def set_top_page_tweets
+    # 空の結果セットもページネーションオブジェクトとして扱う
+    @recommended_tweets ||= Tweet.none.page(params[:page])
+    @following_tweets ||= Tweet.none.page(params[:page])
+    if params[:tab] == 'following'
+      followed_user_id = User.find_by(email: 'test-prd02@gmail.com')
+      @following_tweets = Tweet.following_tweets(followed_user_id, params[:page])
+    else
+      @recommended_tweets = Tweet.recommended_tweets(params[:page])
+    end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,17 +7,7 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @tweet = Tweet.new
-
-    # 空の結果セットもページネーションオブジェクトとして扱う
-    @recommended_tweets ||= Tweet.none.page(params[:page])
-    @following_tweets ||= Tweet.none.page(params[:page])
-    if params[:tab] == 'following'
-      # フォローしているユーザーのツイート一覧
-      followed_user_id = User.find_by(email: 'test-prd02@gmail.com')
-      @following_tweets = Tweet.following_tweets(followed_user_id, params[:page])
-    else
-      # おすすめツイート
-      @recommended_tweets = Tweet.recommended_tweets(params[:page])
-    end
+    # ▼他のコントローラからもトップページを描画するので、共通化した
+    set_top_page_tweets
   end
 end
