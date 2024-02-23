e20f07e141516c0ea053c9b96f9dc0db7972c57d
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-23 12:30:37 +0900

    [Add] コメント投稿の雛形 作成

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_045057) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
