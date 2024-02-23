db660bc98fe65a49f7b16e586aeb6933ca2eae52
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2024-02-23 16:15:05 +0900

    [Update] ユーザー/ツイートモデルとコメントモデルを関連付ける

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :avatar_image
   has_one_attached :profile_image
 
