9edae1c77e1fabb7c165f65d5d8b87f035e0ed5b
森下勇樹 <>
2024-03-02 10:51:45 +0900

    [Refactor] usernameをユーザ登録時に自動生成するようにして、そのデータを参照するように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,17 +17,20 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      logger.debug "authの中身: #{auth.info.inspect}"
-      logger.debug "ユーザー属性: #{user.attributes.inspect}"
       user.name = SecureRandom.alphanumeric(7)
       user.email = auth.info.email.presence || "dummy+#{auth.uid}@example.com"
       user.password = Devise.friendly_token[0, 20]
       user.birthday = Date.new(1900, 1, 1) # デフォルトの誕生日（必要に応じて変更）
       user.phone_number = "090#{rand(10_000_000..99_999_999)}"
+      user.username = "@#{SecureRandom.alphanumeric(7)}"
     end
   end
 
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def self.create_username
+    "@#{SecureRandom.alphanumeric(7)}"
   end
 end
