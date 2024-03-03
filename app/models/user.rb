3b35b698eb57f1534a8ffb94601851ac5634346d
森下勇樹 <>
2024-03-03 18:47:00 +0900

    test commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # test
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
