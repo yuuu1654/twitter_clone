10eae49bff3522a50f5f74dcd42461535eeb1cec
森下勇樹 <>
2024-03-12 09:13:14 +0900

    [Update] バリデーション追加

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,4 +4,7 @@ class Follow < ApplicationRecord
   # Userクラスと関連付けされていることを明示
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+  validates :follower_id, uniqueness: { scope: :followed_id }
 end
