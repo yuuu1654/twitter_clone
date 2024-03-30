e168100d737d0f7fed9f04553eff4c5da7d222c6
森下勇樹 <>
2024-03-30 16:09:02 +0900

    [Update] メッセージにroom_idを持たせる

---
+++ db/migrate/20240305070230_add_room_id_to_messages.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddRoomIdToMessages < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :messages, :room, null: false, foreign_key: true
+  end
+end
