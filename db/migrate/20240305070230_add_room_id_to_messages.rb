44f511d703f779341eb040220a4d4f27b188afc0
森下勇樹 <>
2024-03-30 19:13:34 +0900

    [Update] rubocopエラーの為、null: false指定をやめる
    デフォルトを指定しろと言われたが、外部キーのデフォルトに意味のあるデフォルトを指定できないので、ここで指定せずバリデーションを設定

--- db/migrate/20240305070230_add_room_id_to_messages.rb
+++ db/migrate/20240305070230_add_room_id_to_messages.rb
@@ -2,6 +2,6 @@
 
 class AddRoomIdToMessages < ActiveRecord::Migration[7.0]
   def change
-    add_reference :messages, :room, null: false, foreign_key: true
+    add_reference :messages, :room, foreign_key: true
   end
 end
--- db/migrate/20240306062038_add_user_id_to_rooms.rb
+++ db/migrate/20240306062038_add_user_id_to_rooms.rb
@@ -2,6 +2,6 @@
 
 class AddUserIdToRooms < ActiveRecord::Migration[7.0]
   def change
-    add_reference :rooms, :user, null: false, foreign_key: true
+    add_reference :rooms, :user, foreign_key: true
   end
 end
