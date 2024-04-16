792b35c3cfd1b41a9338524b49bfd52ea6d2f20c
森下勇樹 <>
2024-04-16 14:12:58 +0900

    [Add] 通知作成・削除用のコントローラ

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class NotificationsController < ApplicationController
+  before_action :logged_in_user?, only: [:index]
+
+  def index
+    @notifications = current_user.notifications.order(created_at: :desc)
+    @notifications.where(checked: false).find_each do |notification|
+      notification.update(checked: true)
+    end
+  end
+
+  def destroy
+    @notifications = current_user.notifications.destroy_all
+    redirect_to notifications_path
+  end
+end
