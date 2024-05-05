653ca9d3c6b9288edb5831ca842925ea2ebcfee6
森下勇樹 <>
2024-05-05 17:02:45 +0900

    [Add] railsコマンドで生成されてコミットし忘れたファイルたち

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# require 'rails_helper'
+
+# RSpec.describe NotificationMailer, type: :mailer do
+#   describe 'notification_email' do
+#     let(:mail) { described_class.notification_email }
+
+#     it 'renders the headers' do
+#       expect(mail.subject).to eq('Notification email')
+#       expect(mail.to).to eq(['to@example.org'])
+#       expect(mail.from).to eq(['from@example.com'])
+#     end
+
+#     it 'renders the body' do
+#       expect(mail.body.encoded).to match('Hi')
+#     end
+#   end
+# end
